f984632101c03725c60fb58f3cd9ef89bf0fd0d3
AvivSarig <avivs15@gmail.com>
2023-06-01 19:05:13 +0300

    changes to throttling function, tests added for throttling module

---
+++ tests/requestThrottling.test.js
@@ -0,0 +1,38 @@
+import { throttle } from '../js/requestThrottling.js';
+
+describe('throttle', () => {
+    let func;
+    let throttledFunc;
+
+    beforeEach(() => {
+        func = jest.fn();
+        throttledFunc = throttle(func, 1000);
+        jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+        jest.useRealTimers();
+    });
+
+    test('non-throttled two calls', () => {
+        throttledFunc();
+        jest.advanceTimersByTime(1010);
+        throttledFunc();
+        expect(func).toHaveBeenCalledTimes(2);
+    });
+
+    test('throttled three calls', () => {
+        throttledFunc();
+        throttledFunc();
+        throttledFunc();
+        expect(func).toHaveBeenCalledTimes(1);
+    })
+
+    test('call after two throttled call', () => {
+        throttledFunc();
+        throttledFunc();
+        jest.advanceTimersByTime(1000);
+        throttledFunc();
+        expect(func).toHaveBeenCalledTimes(2);
+    })
+});
